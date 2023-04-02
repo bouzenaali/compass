8f9507e9b4fd5eebaf9e07608457a7dab33abe45
bouzenaali <Alibouzena16@gmail.com>
2023-04-02 23:41:26 +0100

    feat : add the accounts models  (#1)

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Person():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return self.name
+
+
+class Student(Person):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+    def __str__(self):
+        return self.name + " " + str(self.grade)
+    
+
+class Teacher(Person):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+    def __str__(self):
+        return self.name + " " + self.subject
+    
+
+class admin():
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+    def __str__(self):
+        return self.name
