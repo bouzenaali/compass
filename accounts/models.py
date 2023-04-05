26e6f06cb18520b48e66370c40adc5d03f8dd42e
bouzenaali <Alibouzena16@gmail.com>
2023-04-05 10:50:40 +0100

    feat: add password and is_present fields to accounts/models

--- accounts/models.py
+++ accounts/models.py
@@ -5,13 +5,14 @@ class Person(models.Model):
     name = models.CharField(max_length=255)
     phone = models.FloatField()
     mail = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
     
 class Student(Person):
     courses = models.ManyToManyField('courses.Course', related_name='students')
-  
+    is_present = models.BooleanField(default=False)
     def __str__(self):
         return self.name
     
