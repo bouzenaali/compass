575b5d43f11c6586db139537cc4a61ba6eecca79
bouzenaali <Alibouzena16@gmail.com>
2023-04-07 12:05:22 +0100

    fix: don't work yet

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from django.http import JsonResponse
 from django.contrib.auth.models import Group
 from django.contrib.auth.decorators import user_passes_test
 
-@user_passes_test(lambda u: u.groups.filter(name__in=['admins', 'superUsers']).exists())
+# @user_passes_test(lambda u: u.groups.filter(name__in=['admins', 'superUsers']).exists())
 def create_teacher(request):
     name = request.POST.get('name')
     phone = request.POST.get('phone')
@@ -22,14 +22,14 @@ def create_teacher(request):
     user.is_superuser = False 
     user.save()
     
-    # Add the user to the teacher group
-    teacher_group = Group.objects.get(name='teachers')
-    teacher_group.user_set.add(user)
+    # # Add the user to the teacher group
+    # teacher_group = Group.objects.get(name='teachers')
+    # teacher_group.user_set.add(user)
 
     # Return a success message
     return JsonResponse({'success': True})
 
-@user_passes_test(lambda u: u.groups.filter(name='superUsers').exists())
+# @user_passes_test(lambda u: u.groups.filter(name='superUsers').exists())
 def create_superUser(request):
     name = request.POST.get('name')
     phone = request.POST.get('phone')
@@ -46,14 +46,14 @@ def create_superUser(request):
     user.is_superuser = True 
     user.save()
 
-    # Add the user to the superuser group
-    superuser_group = Group.objects.get(name='superusers')
-    superuser_group.user_set.add(user)
+    # # Add the user to the superuser group
+    # superuser_group = Group.objects.get(name='superusers')
+    # superuser_group.user_set.add(user)
 
     # Return a success message
     return JsonResponse({'success': True})
 
-@user_passes_test(lambda u: u.groups.filter(name='superUsers').exists())
+# @user_passes_test(lambda u: u.groups.filter(name='superUsers').exists())
 def create_admin(request):
     name = request.POST.get('name')
     phone = request.POST.get('phone')
@@ -70,15 +70,15 @@ def create_admin(request):
     user.is_superuser = False 
     user.save()
 
-    # Add the user to the admin group
-    admin_group = Group.objects.get(name='admins')
-    admin_group.user_set.add(user)
+    # # Add the user to the admin group
+    # admin_group = Group.objects.get(name='admins')
+    # admin_group.user_set.add(user)
 
     # Return a success message
     return JsonResponse({'success': True})
 
 
-@user_passes_test(lambda u: u.groups.filter(name='teachers').exists())
+# @user_passes_test(lambda u: u.groups.filter(name='teachers').exists())
 def edit_student_attendance(request, student_id):
     # modifier l'absence et la presence des etudiants
     # il faut voir qvel es que l'etudiant ni n le group ines negh khati
