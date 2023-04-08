ffa6b65152743b322e9da5cd9fded937b860153e
bouzenaali <Alibouzena16@gmail.com>
2023-04-08 13:55:23 +0100

    feat: add the auth app

--- compass/urls.py
+++ compass/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('courses.urls')),
+    path('api/', include('accounts.urls')),
+    path('api/', include('timetable.urls')),
+    path('api/', include('dashboard.urls')),
+    path('api/',include('authentication.urls')),
 ]
