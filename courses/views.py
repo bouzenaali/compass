f0670a0bf3b5d5c98ef60b8d7ff98cdb7fb7ae85
bouzenaali <Alibouzena16@gmail.com>
2023-04-23 15:25:56 +0100

    feat: add permissions to the courses views

--- courses/views.py
+++ courses/views.py
@@ -4,10 +4,23 @@ from . models import *
 from rest_framework import generics
 from rest_framework.views import APIView 
 from django.http.response import JsonResponse
+from django.contrib.auth.models import Group, Permission
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import permission_required
 
-# Create your views here.
+
+
+# Create the groups
+teachers_group, created = Group.objects.get_or_create(name='teachers')
+admins_group, created = Group.objects.get_or_create(name='admins')
+
+# Add the permissions to the groups
+admins_group.permissions.set(Permission.objects.filter(codename__in=['change_Course', 'change_Level', 'change_Group', 'change_Session','add_Course', 'add_Level', 'add_Group', 'add_Session','delete_Course', 'delete_Level', 'delete_Group', 'delete_Session','view_Course', 'view_Level', 'view_Group', 'view_Session']))
+teachers_group.permissions.set(Permission.objects.filter(codename__in=['view_Course', 'view_Level', 'view_Group', 'view_Session']))
+
 
 #create course
+@method_decorator(permission_required('courses.change_Course'),name='dispatch')
 class addCourse(APIView):
     def post(self, request):
         name = request.data.get('name')
@@ -41,39 +54,51 @@ class addCourse(APIView):
 
 
 # views that lists existing courses/levels/groups/sessions
-
+@method_decorator(permission_required('courses.view_Course'),name='dispatch')
 class CourseList (generics.ListAPIView):
      queryset=Course.objects.all()
      serializer_class=CourseSerializer
 
+@method_decorator(permission_required('courses.view_Level'),name='dispatch')
 class LevelList (generics.ListAPIView):
      queryset=Level.objects.all()
      serializer_class=LevelSerializer
      
-
+@method_decorator(permission_required('courses.view_Group'),name='dispatch')
 class GroupList (generics.ListAPIView):
      queryset=Group.objects.all()
      serializer_class=GroupSerializer
      
-
+@method_decorator(permission_required('courses.view_Session'),name='dispatch')
 class SessionList (generics.ListAPIView):
      queryset=Session.objects.all()
      serializer_class=SessionSerializer
       
 #views that retrieve/update/destroy a course/level/session/group
-
+@method_decorator(permission_required('courses.change_Course'),name='dispatch')
+@method_decorator(permission_required('courses.delete_Course'),name='dispatch')
+@method_decorator(permission_required('courses.view_Course'),name='dispatch')
 class CourseRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset =Course.objects.all()
     serializer_class = CourseSerializer
 
+@method_decorator(permission_required('courses.change_Level'),name='dispatch')
+@method_decorator(permission_required('courses.delete_Level'),name='dispatch')
+@method_decorator(permission_required('courses.view_Level'),name='dispatch')
 class LevelRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Level.objects.all()
     serializer_class = LevelSerializer
 
+@method_decorator(permission_required('courses.change_Session'),name='dispatch')
+@method_decorator(permission_required('courses.delete_Session'),name='dispatch')
+@method_decorator(permission_required('courses.view_Session'),name='dispatch')
 class SessionRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Session.objects.all()
     serializer_class = SessionSerializer
 
+@method_decorator(permission_required('courses.change_Group'),name='dispatch')
+@method_decorator(permission_required('courses.delete_Group'),name='dispatch')
+@method_decorator(permission_required('courses.view_Group'),name='dispatch')
 class GroupRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
